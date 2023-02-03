2fe4262d3785add2531eabee90cea69e00e5a6ee
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-02-03 15:11:23 +0545

    working with the shop header

--- script.js
+++
@@ -1,7 +0,0 @@
-var bgImage= document.querySelector(".bgImage");
-var bgImageHeight= bgImage.clientHeight;
-console.log(bgImageHeight);
-window.addEventListener("resize", function(){
-    bgImageHeight= bgImage.clientHeight;
-})
-console.log(bgImageHeight);
